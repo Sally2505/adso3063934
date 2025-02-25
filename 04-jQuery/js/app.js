bd84c15c582fb1c588e0edc7ade4747619cdbc62
sally2505 <ssalomee182103@gmail.com>
2025-02-25 15:23:13 -0500

    To do list done

--- 04-jQuery/js/app.js
+++ 04-jQuery/js/app.js
@@ -1,8 +1,18 @@
 $(function () {
-    //count Tasks//
-    countTasks()
-    countRemains()
-    //add Task//
+
+    //check localStorage
+
+    if (localStorage.getItem('To do list') != null) {
+        $('.list').html(localStorage.getItem('To do list'))
+        countTasks()
+        countRemains()
+    } else {
+        //count Tasks & Remains
+        countTasks()
+        countRemains()
+    }
+
+    //add Task
     $('footer').on('click', '#add', function () {
         if ($('#input-task').val().length > 0) {
 
@@ -20,21 +30,26 @@ $(function () {
             alert('please! Enter a Task')
         }
     })
-    $('body').on('click', 'input[type=checkbox]', function () {
 
-        if ($(this).prop('checked')) {
-            $(this).parent().addClass('checked')
-        } else {
-            $(this).parent().removeClass('checked')
-        }
-        countRemains()
-    })
-    $('body').on('click', 'article button', function(){
-        $(this).closest('article').remove()
-        countTasks()
-        countRemains()
-    })
 })
+$('body').on('click', 'input[type=checkbox]', function () {
+    // if checked
+    if ($(this).prop('checked')) {
+        $(this).attr('checked', true)
+        $(this).parent().addClass('checked')
+    } else {
+        $(this).attr('checked', false)
+        $(this).parent().removeClass('checked')
+    }
+    countRemains()
+})
+//Remove Tasks
+$('body').on('click', 'article button', function () {
+    $(this).closest('article').remove()
+    countTasks()
+    countRemains()
+})
+
 
 function countTasks() {
     $('.num-tasks').text($('article').length)
@@ -45,4 +60,17 @@ function countRemains() {
     $remain = Math.abs($('.checked').length - $('article').length)
     $('.num-remains').text($remain)
     $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
+    //set localStorage
+    localStorage.setItem('To do list', $('.list').html())
 }
+
+function reset() {
+    $('.list').html('')
+    localStorage.removeItem('To do list')
+    countTasks()
+    countRemains()
+}
+
+$('main').on('click', '#reset', function () {
+    reset()
+})
